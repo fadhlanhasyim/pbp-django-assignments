0e45105bd9959819a23b22c46b2860e409056392
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-09-11 15:49:10 +0700

    Setup for Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+        'data_item_katalog': data_item_katalog,
+        'nama': 'Fadhlan Hasyim',
+        'id': '2106652215',
+    }
+    return render(request, 'katalog.html', context)
