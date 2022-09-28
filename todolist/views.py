bd8f0472348a423aee5109a93114a8a7b103ebaf
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-09-28 18:44:14 +0700

    Second submit tugas 4

--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,3 @@
-from turtle import title
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
